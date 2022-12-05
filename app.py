d000e0181bbb7110008fd302b0df263435998bc8
NGUYỄN HỮU ĐĂNG <ducbmt2009@gmail.com>
2022-12-05 17:24:31 +0700

    response product list base on fav

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from flask import Flask, jsonify, render_template, request
 from flask_mysqldb import MySQL
-from tabulate import tabulate
+# from tabulate import tabulate
 
 app = Flask(__name__)
 
@@ -19,14 +19,14 @@ mysql = MySQL(app)
 @app.route('/recommend/<userId>', methods=['GET'])
 def recommend(userId):
     # query
-    getAllProduct_query_string = "select p.id, category_id, name, t.price from products p, types t where p.id = t.product_id group by p.id"
+    getall_product_query_string = "select p.id, category_id, name, t.price from products p, types t where p.id = t.product_id group by p.id"
 
-    getAllWishList_query_string = "select p.id ,p.category_id, p.name, w.price from wishlists w, products p where w.product_id = p.id and w.user_id=" + userId
+    getall_wishlist_query_string = "select p.id ,p.category_id, p.name, w.price from wishlists w, products p where w.product_id = p.id and w.user_id=" + userId
 
     cursor = mysql.connection.cursor()
 
     # query all products in DB
-    cursor.execute(getAllProduct_query_string)
+    cursor.execute(getall_product_query_string)
 
     # turn query data into table
     row_headers = [x[0] for x in cursor.description]
@@ -40,14 +40,14 @@ def recommend(userId):
     table_product_data = pd.DataFrame(data=json_data)
 
     # get all product id in wishlist
-    cursor.execute(getAllWishList_query_string)
+    cursor.execute(getall_wishlist_query_string)
     data = cursor.fetchall()
 
     # array contains all productId in wishlist
-    listProductId = []
+    list_product_id = []
     for product in data:
-        listProductId.append(product[0])
-    # print(listProductId)
+        list_product_id.append(product[0])
+    # print(list_product_id)
 
     # print(table_product_data)
     # Tạo ra một column để gom nhóm các features
@@ -79,34 +79,41 @@ def recommend(userId):
     def getProductName(index):
         return table_product_data[table_product_data.index == index]["name"].values[0]
 
-    recommendList = []
+    recommend_list = []
     # List tương đồng cosine cho các movies của input movie
-    for Id in listProductId:
-        similarProducts = list(enumerate(cosineSimilarity[getIndexById(Id)]))
-        sortedSimilarProduct = sorted(similarProducts, key = lambda x: x[1], reverse = True)
-        # print(sortedSimilarProduct)
+    for Id in list_product_id:
+        similar_products = list(enumerate(cosineSimilarity[getIndexById(Id)]))
+        sorted_similar_products = sorted(similar_products, key = lambda x: x[1], reverse = True)
+        # print(sorted_similar_products)
         i = 0
-        for product in sortedSimilarProduct:
+        for product in sorted_similar_products:
             if (getProductName(product[0]) != getProductName(getIndexById(Id))):
                 # print(getProductName(product[0]))
-                # if (len(recommendList) > 100):
+                # if (len(recommend_list) > 100):
                 #     break
-                getProductById_query_string = "select p.id, p.name, p.image, p.avg_rating, t.price from products p, types t where t.product_id = p.id and p.id=" + Id + "group by p.id"
-                cursor.execute(getAllProduct_query_string)
+                pros_query_string = "select p.id, p.name, p.image, p.avg_rating, t.price from products p, types t where t.product_id = p.id and p.id = " + str(Id) + " group by p.id"
+                cursor.execute(pros_query_string)
                 data = cursor.fetchall()
+                
                 print(data)
 
-                recommendList.append(getProductName(product[0]))
+                row_headers = [x[0] for x in cursor.description]
+
+                json_recommend_list = []
+
+                for product in data: 
+                    json_recommend_list.append(dict(zip(row_headers, product)))
+
+                recommend_list.append(dict(zip(row_headers, product)))
+
                 i = i + 1
                 if i > 4:
                     break
+
+    print(recommend_list)
     
-
-
-    # print(recommendList)
-
     cursor.close()
 
-    return jsonify({'wishlist': json_data})
+    return jsonify({'wishlist': recommend_list})
 
 app.run(host='localhost', port=8081)
